b8cc9ce573b09448d13cc2ddc2146ec4d83d779a
Andrew Jones <joneseyit@protonmail.com>
2019-03-04 08:16:26 -0500

    Got model relationshionships setup

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,2 +1,7 @@
 class Invitation < ApplicationRecord
+    belongs_to :user
+    belongs_to :invited, className: "User"
+    belongs_to :game
+
+    validates :invited_id, uniqueness: {scope: :game_id}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :user_games
+    has_many :games, through: user_games
+    has_many :invitations
+    has_many : inviteds, className: "User", foreign_key: "invited_id"
 end
